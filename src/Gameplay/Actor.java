d27724daec5bda62a2553591f6bebde2dde34f53
magyk81 <magyk81@gmail.com>
2018-06-18 22:28:34 -0600

    Added Jump class

--- src/Gameplay/Actor.java
+++ src/Gameplay/Actor.java
@@ -3,6 +3,9 @@ package Gameplay;
 import Util.Print;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.World;
+import org.jbox2d.dynamics.contacts.ContactEdge;
+
+import java.util.ArrayList;
 
 /**
  * Created by Joel on 6/14/2018.
@@ -135,6 +138,14 @@ public class Actor extends Entity
         return newVel;
     }
 
+    private Vec2 getNewVel()
+    {
+        Vec2 oldVel = body.getLinearVelocity();
+        float xVel = oldVel.x;
+        float yVel = oldVel.y - jumpSpeed;
+        return new Vec2(xVel, yVel);
+    }
+
     private boolean pressingLeft = false;
     private boolean pressingRight = false;
     private boolean pressingUp = false;
@@ -183,56 +194,96 @@ public class Actor extends Entity
             else dirVertical = null; }
         pressingDown = pressed; }
     void pressJump(boolean pressed) {
-        //currentJump = currentJump.trigger(state.jumpSource(), pressed);
-        // TODO: Parameter for trigger() should come from the State enum
+        if (pressingJump == pressed) return;
+        jump = jump.trigger(pressed);
         pressingJump = pressed; }
 
-    /* TODO: This class needs a lot of work */
     private class Jump
     {
+        // TODO: Make the armed variable have a short time limit
         private boolean armed;
-        private boolean started = false;
+        //private boolean started = false;
         Jump(boolean ready)
         {
             armed = ready;
         }
         Jump() { start(); }
+        /* Called whenever the jump key is pressed orr released */
         Jump trigger(boolean pressed)
         {
-            if (state.isAirborne())
-            {
-                return new Jump(pressed);
-            }
-            return new Jump();
+            /* If pressed in the air, the Jump becomes armed.
+             * If released in the air, the Jump becomes unarmed
+             * and unstarted. */
+            if (state.isAirborne()) return new Jump(pressed);
+            /* If pressed on the ground, the Jump starts.
+             * If released on the ground, the Jump becomes unarmed
+             * and unstarted. */
+            return pressed ? new Jump() : new Jump(false);
         }
+        /* Called whenever the player hits a surface */
         Jump trigger()
         {
-            return this;
+            /* If armed when hitting a surface, the Jump starts. */
+            if (armed) return new Jump();
+            /* If unarmed when hitting a surface, the Jump becomes
+             * unarmed and unstarted. */
+            return new Jump(false);
         }
-        void start() {}
+        void start()
+        {
+            armed = false;
+            //started = true;
+            body.setLinearVelocity(getNewVel());
+        }
     }
 
     private enum Direction {
-        UP { boolean up() { return true; } boolean vertical() { return true; } },
-        LEFT { boolean left() { return true; } boolean horizontal() { return true; } },
-        DOWN { boolean down() { return true; } boolean vertical() { return true; } },
-        RIGHT { boolean right() { return true; } boolean horizontal() { return true; } },
-        /*UP_IN { boolean in() { return true; } boolean up() { return true; } boolean vertical() { return true; } },
-        LEFT_IN { boolean in() { return true; } boolean left() { return true; } boolean horizontal() { return true; } },
-        DOWN_IN { boolean in() { return true; } boolean down() { return true; } boolean vertical() { return true; } },
-        RIGHT_IN{ boolean in() { return true; } boolean right() { return true; } boolean horizontal() { return true; } },*/
-        ERROR;
-        boolean in() { return false; }
-        boolean up() { return false; }
-        boolean left() { return false; }
-        boolean down() { return false; }
-        boolean right() { return false; }
+        UP { boolean vertical() { return true; } },
+        LEFT { boolean horizontal() { return true; } },
+        DOWN { boolean vertical() { return true; } },
+        RIGHT { boolean horizontal() { return true; } };
         boolean vertical() { return false; }
         boolean horizontal() { return false; } }
 
+    /**
+     * Returns which direction the other entity is in relation to this Actor.
+     */
+    private Direction inBoundsHoriz(Entity other, boolean checkWithin)
+    {
+        if (!checkWithin)
+        {
+            if (getRightEdge() < other.getLeftEdge()) return Direction.RIGHT;
+            if (getLeftEdge() > other.getRightEdge()) return Direction.LEFT;
+            return null;
+        }
+
+        if (getRightEdge() < other.getRightEdge()) return Direction.RIGHT;
+        if (getLeftEdge() > other.getLeftEdge()) return Direction.LEFT;
+
+        Print.red("Error: Could not determine relative position");
+        return null;
+    }
+    /**
+     * Returns which direction the other entity is in relation to this Actor.
+     */
+    private Direction inBoundsVert(Entity other, boolean checkWithin)
+    {
+        if (!checkWithin)
+        {
+            if (getBottomEdge() < other.getTopEdge()) return Direction.DOWN;
+            if (getTopEdge() > other.getBottomEdge()) return Direction.UP;
+            return null;
+        }
+
+        if (getBottomEdge() < other.getBottomEdge()) return Direction.DOWN;
+        if (getTopEdge() > other.getTopEdge()) return Direction.UP;
+
+        Print.red("Error: Could not determine relative position");
+        return null;
+    }
+
     private enum State {
-        PRONE_UP { boolean isGrounded() { return true; } },
-        PRONE_DOWN { boolean isGrounded() { return true; } },
+        PRONE { boolean isGrounded() { return true; } },
         RISE { boolean isAirborne() { return true; } },
         FALL { boolean isAirborne() { return true; } },
         STAND { boolean isGrounded() { return true; } },
@@ -245,4 +296,57 @@ public class Actor extends Entity
         boolean isOnWall() { return false; }
         boolean isAirborne() { return false; }
         boolean isGrounded() { return false; } }
+
+    void setState(State state)
+    {
+        /* Temporary */
+        Print.blue("Changing from state \"" + this.state + "\" to \"" + state + "\"");
+    }
+
+    void triggerContacts(ArrayList<Entity> entities)
+    {
+        boolean withinBlockHoriz = false;
+        boolean withinBlockVert = false;
+        int groundsCounted = 0;
+        int wallsCounted = 0;
+        ContactEdge contactEdge = body.getContactList();
+        while (contactEdge != null)
+        {
+            for (Entity entity : entities)
+            {
+                if (contactEdge.other == entity.body && contactEdge.contact.isTouching())
+                {
+                    entity.triggered = true;
+                    triggered = true;
+
+                    Direction horizBound = inBoundsHoriz(entity, false);
+                    Direction vertBound = inBoundsVert(entity, false);
+                    if (horizBound == null)
+                    {
+                        horizBound = inBoundsHoriz(entity, true);
+                        withinBlockHoriz = true;
+                    }
+                    if (vertBound == null)
+                    {
+                        vertBound = inBoundsVert(entity, true);
+                        withinBlockVert = true;
+                    }
+
+                    if (withinBlockHoriz && vertBound == Direction.DOWN) groundsCounted++;
+                    else if (withinBlockVert) wallsCounted++;
+                }
+            }
+            contactEdge = contactEdge.next;
+        }
+        if (inWater()) setState(State.SWIM);
+        else if (groundsCounted > 0) setState(State.STAND);
+        else if (wallsCounted > 0) setState(State.WALL_STICK);
+        else setState(State.FALL);
+    }
+
+    boolean inWater()
+    {
+        /* Temporary */
+        return false;
+    }
 }
