482e0ce17c6cccca6df46fac972e7d4db4e09291
castellanos70 <castellanos70@gmail.com>
2019-02-02 15:44:25 -0700

    finished convexHull(Vec2[] poly1, Vec2[] poly2)

--- src/Util/PolygonIntersection.java
+++ src/Util/PolygonIntersection.java
@@ -99,9 +99,18 @@ public class PolygonIntersection
 
 
 
-
     //==================================================================================================================
     // Given two sets of points, this method returns the convex hull of the points.
+    //
+    // There is no assumption about the ordering of the points in either given polygon.
+    // There is no assumption of uniqueness of points.
+    // The returned array will be the smallest number of points that define the convex hull in clock-wise order
+    // within an error = epsilon.
+    //
+    // A new array is returned with all new data elements. Thus, if the input polygons can later be changed without
+    // changing the data values in the returned polygon.
+    // The data in the arrays passed into this method is not changed.
+    //
     // The convex hull of a set X of points in the Euclidean plane is the smallest convex set that contains X.
     //   For instance, when X is a bounded subset of the plane, the convex hull may be visualized as the shape
     //   enclosed by a rubber band stretched around X.
@@ -110,42 +119,138 @@ public class PolygonIntersection
     // complexity O(n log n). It is named after Ronald Graham, who published the original algorithm in 1972.
     // The algorithm finds all vertices of the convex hull ordered along its boundary.
     // It uses a stack to detect and remove concavities in the boundary efficiently.
-    // The algorithm sorts the points by y (and or equal y, sorts by x). This sorting is the slowest part of the
-    //   algorithm, but since in this case there are a small number of points, I just use bubble sort, O(n*n).
-    // https://en.wikipedia.org/wiki/Graham_scan
+    //
+    // The first step in this algorithm is to find the point with the lowest y-coordinate.
+    // If the lowest y-coordinate exists in more than one point in the set, the point with the lowest x-coordinate
+    // out of the candidates should be chosen.
+    // Call this point P. This step takes O(n), where n is the number of points in question.
+    //
+    // Next, the points must be sorted in increasing order of the angle they and the point P make with the x-axis.
+    // Since the number of points in this app is expected to be small, I just use bubble sort, O(n*n).
+    //
+    // For each point, it is first determined whether traveling from the two points immediately preceding this point
+    // constitutes making a left turn or a right turn. If a right turn, the second-to-last point is not part of the
+    // convex hull, and lies 'inside' it. The same determination is then made for the set of the latest point and the
+    // two points that immediately precede the point found to have been inside the hull, and is repeated until a
+    // "left turn" set is encountered, at which point the algorithm moves on to the next point in the set of points
+    // in the sorted array minus any points that were found to be inside the hull.
+    //
+    // see: https://en.wikipedia.org/wiki/Graham_scan
     //==================================================================================================================
     public static Vec2[] convexHull(Vec2[] poly1, Vec2[] poly2)
     {
+        double epsilon = 0.00001;
+
         //sort points by y-coordinate
         int totalPoints = poly1.length + poly2.length;
-        ArrayList<Vec2> pointList = new ArrayList<>(totalPoints);
 
-        //Add all points to arrayList
+        //Merge all points from both polygons into a single array.
+        Vec2[] poly3 = new Vec2[totalPoints];
         for (int i=0; i<poly1.length; i++)
         {
-            pointList.add(poly1[i]);
+            poly3[i] = new Vec2(poly1[i]);
         }
         for (int i=0; i<poly2.length; i++)
         {
-            pointList.add(poly2[i]);
+            poly3[i+poly1.length] = new Vec2(poly2[i]);
         }
 
-        //Bubble sort
-        boolean didSwap = true;
+
+        //Move lowest left most point to first element of array
+        int minIdx = -1;
+        float minx = Float.MAX_VALUE;
+        float miny = Float.MAX_VALUE;
         for (int i=0; i<totalPoints; i++)
         {
-            for (int k=0; k<(totalPoints-i)-1; k++)
+            if ((poly3[i].y < miny) || (poly3[i].y == miny && poly3[i].x < minx))
             {
-               Vec2 p1 = pointList.get(k);
-               Vec2 p2 = pointList.get(k+1);
-               if ((p2.y < p1.y) || (p2.y == p1.y && p2.x < p1.x))
-               {
+                minx = poly3[i].x;
+                miny = poly3[i].y;
+                minIdx = i;
+            }
+        }
+        Vec2 tmp = poly3[0];
+        poly3[0] = poly3[minIdx];
+        poly3[minIdx] = tmp;
 
+
+        //Compute the angle each point and the first point make with the x-axis.
+        double[] angle = new double[totalPoints];
+        for (int i=0; i<totalPoints; i++)
+        {
+            //cos of angle by dot product
+            double dx = poly3[i].x - poly3[0].x;
+            double dy = poly3[i].y - poly3[0].y;
+            double r  = Math.sqrt(dx*dx + dy*dy);
+            if (r<epsilon) angle[i] = -1;
+            else
+            {
+                angle[i] = 1 - dx / r;
+            }
+            //System.out.println(i + 1 + ") " + angle[i]);
+        }
+
+
+        //Bubble sort in increasing order of the angle each point and the first point make with the x-axis.
+        boolean didSwap = true;
+        for (int i=1; i<totalPoints; i++)
+        {
+            for (int k=1; k<(totalPoints-i); k++)
+            {
+               if (angle[k]>angle[k+1])
+               {
+                   tmp = poly3[k];
+                   poly3[k] = poly3[k+1];
+                   poly3[k+1] = tmp;
+
+                   double tmpAngle = angle[k];
+                   angle[k] = angle[k+1];
+                   angle[k+1] = tmpAngle;
+                   didSwap = true;
                }
+            }
+            if (!didSwap) break;
+        }
+
+        //Add the first point to the hull and the next point that is far enough from the first point as to be interesting.
+        ArrayList<Vec2> hullList = new ArrayList<>(totalPoints);
+        hullList.add(poly3[0]);
+        //System.out.println("Add ("+poly3[0].x+", "+poly3[0].y+")");
+        int idx;
+        for (idx=1; idx<totalPoints-1; idx++)
+        {
+            float dx = Math.abs(poly3[0].x - poly3[idx].x);
+            float dy = Math.abs(poly3[0].y - poly3[idx].y);
+            if ((dx > epsilon) || (dy > epsilon))
+            {
+                break;
             }
         }
 
-        return poly1;
+        hullList.add(poly3[idx]);
+        //System.out.println("Add ("+poly3[idx].x+", "+poly3[idx].y+")");
+        //Add points to the hull that make a left turn.
+        for (int i=idx+1; i<totalPoints; i++)
+        {
+            Vec2 p3 = poly3[i];
+            while (hullList.size() > 2)
+            {
+                Vec2 p1 = hullList.get(hullList.size() - 2);
+                Vec2 p2 = hullList.get(hullList.size() - 1);
+
+                float a = (p2.x - p1.x) * (p3.y - p1.y);
+                float b = (p2.y - p1.y) * (p3.x - p1.x);
+                if (a - b <= epsilon)
+                {
+                    hullList.remove(hullList.size() - 1);
+                    //System.out.println("Remove (" + p2.x + ", " + p2.y + ")");
+                }
+                else break;
+            }
+            hullList.add(p3);
+            //System.out.println("Add (" + p3.x + ", " + p3.y + ")");
+        }
+        return hullList.toArray(new Vec2[hullList.size()]);
     }
 
 
@@ -204,6 +309,30 @@ public class PolygonIntersection
 
         assert(isIntersect(sword, tri1) == false);
         assert(isIntersect(sword, tri2) == true);
+
+
+        Vec2[] poly =
+                {
+                        new Vec2(40,0),
+                        new Vec2(50,10),
+                        new Vec2(20,20),
+                        new Vec2(50,50),
+                        new Vec2(70,30),
+                        new Vec2(50,0),
+                        new Vec2(0,40),
+                        new Vec2(0,0),
+                        new Vec2(60,-20),
+                        new Vec2(20,60)
+                };
+
+
+
+        Vec2[] hull = convexHull(sword, poly);
+        System.out.println("Convex Hull:");
+        for (int i=0; i<hull.length; i++)
+        {
+            System.out.println(i+" : " + hull[i].x + ", "+hull[i].y);
+        }
 
     }
 }
