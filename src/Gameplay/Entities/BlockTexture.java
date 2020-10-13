2abebd80672a8f8bad6e3d55302008feec3dca44
castellanos70 <castellanos70@gmail.com>
2020-10-12 23:20:48 -0600

    Adjusted block textures

--- src/Gameplay/Entities/BlockTexture.java
+++ src/Gameplay/Entities/BlockTexture.java
@@ -63,62 +63,64 @@ public class BlockTexture
             edgeTextureList[i] = new ArrayList<BlockTexture>();
         }
 
-        blockTextureList = new BlockTexture[44];
+        blockTextureList = new BlockTexture[46];
 
         blockTextureList[0] = new BlockTexture("Left0",  4,0, EdgeType.LEFT);
         blockTextureList[1] = new BlockTexture("Left1",  2,0, EdgeType.LEFT);
         blockTextureList[2] = new BlockTexture("Left2",  2,0, EdgeType.LEFT);
         blockTextureList[3] = new BlockTexture("Left3",  2,0, EdgeType.LEFT);
         blockTextureList[4] = new BlockTexture("Left4",  2,0, EdgeType.LEFT);
-        blockTextureList[5] = new BlockTexture("Left5",  0,0, EdgeType.LEFT);
+        blockTextureList[5] = new BlockTexture("Left5",  3,0, EdgeType.LEFT);
+        blockTextureList[6] = new BlockTexture("Left6",  2,0, EdgeType.LEFT);
 
-        blockTextureList[6] = new BlockTexture("Right0",  0,0, EdgeType.RIGHT);
-        blockTextureList[7] = new BlockTexture("Right1",  0,0, EdgeType.RIGHT);
-        blockTextureList[8] = new BlockTexture("Right2",  0,0, EdgeType.RIGHT);
-        blockTextureList[9] = new BlockTexture("Right3",  0,0, EdgeType.RIGHT);
-        blockTextureList[10] = new BlockTexture("Right4",  0,0, EdgeType.RIGHT);
-        blockTextureList[11] = new BlockTexture("Right5",  0,0, EdgeType.RIGHT);
+        blockTextureList[7] = new BlockTexture("Right0",  0,0, EdgeType.RIGHT);
+        blockTextureList[8] = new BlockTexture("Right1",  0,0, EdgeType.RIGHT);
+        blockTextureList[9] = new BlockTexture("Right2",  0,0, EdgeType.RIGHT);
+        blockTextureList[10] = new BlockTexture("Right3",  0,0, EdgeType.RIGHT);
+        blockTextureList[11] = new BlockTexture("Right4",  0,0, EdgeType.RIGHT);
+        blockTextureList[12] = new BlockTexture("Right5",  0,0, EdgeType.RIGHT);
+        blockTextureList[13] = new BlockTexture("Right6",  0,0, EdgeType.RIGHT);
 
-        blockTextureList[12] = new BlockTexture("Top0",  0,0, EdgeType.TOP);
-        blockTextureList[13] = new BlockTexture("Top1",  0,1, EdgeType.TOP);
-        blockTextureList[14] = new BlockTexture("Top2",  0,1, EdgeType.TOP);
-        blockTextureList[15] = new BlockTexture("Top3",  0,1, EdgeType.TOP);
-        blockTextureList[16] = new BlockTexture("Top4",  0,2, EdgeType.TOP);
-        blockTextureList[17] = new BlockTexture("Top5",  0,2, EdgeType.TOP);
-        blockTextureList[18] = new BlockTexture("Top6",  0,4, EdgeType.TOP);
-        blockTextureList[19] = new BlockTexture("Top7",  0,1, EdgeType.TOP);
-        blockTextureList[20] = new BlockTexture("Top8",  0,2, EdgeType.TOP);
-        blockTextureList[21] = new BlockTexture("Top9",  0,5, EdgeType.TOP);
+        blockTextureList[14] = new BlockTexture("Top0",  0,0, EdgeType.TOP);
+        blockTextureList[15] = new BlockTexture("Top1",  0,1, EdgeType.TOP);
+        blockTextureList[16] = new BlockTexture("Top2",  0,1, EdgeType.TOP);
+        blockTextureList[17] = new BlockTexture("Top3",  0,1, EdgeType.TOP);
+        blockTextureList[18] = new BlockTexture("Top4",  0,2, EdgeType.TOP);
+        blockTextureList[19] = new BlockTexture("Top5",  0,2, EdgeType.TOP);
+        blockTextureList[20] = new BlockTexture("Top6",  0,4, EdgeType.TOP);
+        blockTextureList[21] = new BlockTexture("Top7",  0,1, EdgeType.TOP);
+        blockTextureList[22] = new BlockTexture("Top8",  0,2, EdgeType.TOP);
+        blockTextureList[23] = new BlockTexture("Top9",  0,5, EdgeType.TOP);
 
-        blockTextureList[22] = new BlockTexture("Top-Left0",  2,3, EdgeType.TOP_LEFT);
-        blockTextureList[23] = new BlockTexture("Top-Left1",  3,1, EdgeType.TOP_LEFT);
-        blockTextureList[24] = new BlockTexture("Top-Left2",  0,1, EdgeType.TOP_LEFT);
+        blockTextureList[24] = new BlockTexture("Top-Left0",  2,3, EdgeType.TOP_LEFT);
+        blockTextureList[25] = new BlockTexture("Top-Left1",  3,1, EdgeType.TOP_LEFT);
+        blockTextureList[26] = new BlockTexture("Top-Left2",  3,1, EdgeType.TOP_LEFT);
 
-        blockTextureList[25] = new BlockTexture("Top-Right0",  0,3, EdgeType.TOP_RIGHT);
-        blockTextureList[26] = new BlockTexture("Top-Right1",  0,1, EdgeType.TOP_RIGHT);
-        blockTextureList[27] = new BlockTexture("Top-Right2",  0,1, EdgeType.TOP_RIGHT);
+        blockTextureList[27] = new BlockTexture("Top-Right0",  0,3, EdgeType.TOP_RIGHT);
+        blockTextureList[28] = new BlockTexture("Top-Right1",  0,1, EdgeType.TOP_RIGHT);
+        blockTextureList[29] = new BlockTexture("Top-Right2",  0,3, EdgeType.TOP_RIGHT);
 
-        blockTextureList[28] = new BlockTexture("Corner-Right0",  0,0, EdgeType.CORNER_RIGHT);
-        blockTextureList[29] = new BlockTexture("Corner-Right1",  0,0, EdgeType.CORNER_RIGHT);
-        blockTextureList[30] = new BlockTexture("Corner-Right2",  0,0, EdgeType.CORNER_RIGHT);
+        blockTextureList[30] = new BlockTexture("Corner-Right0",  0,0, EdgeType.CORNER_RIGHT);
+        blockTextureList[31] = new BlockTexture("Corner-Right1",  0,0, EdgeType.CORNER_RIGHT);
+        blockTextureList[32] = new BlockTexture("Corner-Right2",  0,0, EdgeType.CORNER_RIGHT);
 
-        blockTextureList[31] = new BlockTexture("Corner-Left0",  0,0, EdgeType.CORNER_LEFT);
-        blockTextureList[32] = new BlockTexture("Corner-Left1",  0,0, EdgeType.CORNER_LEFT);
-        blockTextureList[33] = new BlockTexture("Corner-Left2",  0,0, EdgeType.CORNER_LEFT);
+        blockTextureList[33] = new BlockTexture("Corner-Left0",  0,0, EdgeType.CORNER_LEFT);
+        blockTextureList[34] = new BlockTexture("Corner-Left1",  0,0, EdgeType.CORNER_LEFT);
+        blockTextureList[35] = new BlockTexture("Corner-Left2",  0,0, EdgeType.CORNER_LEFT);
 
-        blockTextureList[34] = new BlockTexture("Bottom-Left0",  4,0, EdgeType.BOT_LEFT);
-        blockTextureList[35] = new BlockTexture("Bottom-Right0",  0,0, EdgeType.BOT_RIGHT);
+        blockTextureList[36] = new BlockTexture("Bottom-Left0",  4,0, EdgeType.BOT_LEFT);
+        blockTextureList[37] = new BlockTexture("Bottom-Right0",  0,0, EdgeType.BOT_RIGHT);
 
-        blockTextureList[36] = new BlockTexture("Bottom0",  0,0, EdgeType.BOT);
-        blockTextureList[37] = new BlockTexture("Bottom1",  0,0, EdgeType.BOT);
+        blockTextureList[38] = new BlockTexture("Bottom0",  0,0, EdgeType.BOT);
+        blockTextureList[39] = new BlockTexture("Bottom1",  0,0, EdgeType.BOT);
 
-        blockTextureList[38] = new BlockTexture("Ramp-Right18.4-0",  0,0, EdgeType.RAMP_RIGHT18);
-        blockTextureList[39] = new BlockTexture("Ramp-Right18.4-1",  0,0, EdgeType.RAMP_RIGHT18);
-        blockTextureList[40] = new BlockTexture("Ramp-Right18.4-2",  0,0, EdgeType.RAMP_RIGHT18);
+        blockTextureList[40] = new BlockTexture("Ramp-Right18.4-0",  0,0, EdgeType.RAMP_RIGHT18);
+        blockTextureList[41] = new BlockTexture("Ramp-Right18.4-1",  0,0, EdgeType.RAMP_RIGHT18);
+        blockTextureList[42] = new BlockTexture("Ramp-Right18.4-2",  0,0, EdgeType.RAMP_RIGHT18);
 
-        blockTextureList[38] = new BlockTexture("Ramp-Left18.4-0",  0,0, EdgeType.RAMP_LEFT18);
-        blockTextureList[39] = new BlockTexture("Ramp-Left18.4-1",  0,0, EdgeType.RAMP_LEFT18);
-        blockTextureList[40] = new BlockTexture("Ramp-Left18.4-2",  0,0, EdgeType.RAMP_LEFT18);
+        blockTextureList[43] = new BlockTexture("Ramp-Left18.4-0",  0,0, EdgeType.RAMP_LEFT18);
+        blockTextureList[44] = new BlockTexture("Ramp-Left18.4-1",  0,0, EdgeType.RAMP_LEFT18);
+        blockTextureList[45] = new BlockTexture("Ramp-Left18.4-2",  0,0, EdgeType.RAMP_LEFT18);
 
     }
 }
